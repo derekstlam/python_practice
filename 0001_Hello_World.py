fac63d7a86c48a8d737769d33b67b4988356b098
Derek Lam <sheungtak.lam@gmail.com>
2017-09-20 22:09:48 -0400

    add Hello World

---
+++ 0001_Hello_World.py
@@ -0,0 +1,11 @@
+
+print ('Hello World!')
+print ('What is your name?')
+username = input()  #ask for name
+print ('It is good to meet you, ' + username)
+print ('The length of your name is:')
+print (len(username))
+print ('What\'s your age?')
+userage = input()
+print ('You will be ' + str(int(userage) + 1) + ' in a year.')
+
